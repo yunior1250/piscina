b450aa575706ba66ecb8b6896852c1a4f8bc8fa5
Elian Huanca <huancacori@gmail.com>
2023-07-07 17:47:18 -0400

    IA Implementada

--- app/Http/Controllers/Api/ControlPH.php
+++ app/Http/Controllers/Api/ControlPH.php
@@ -15,19 +15,25 @@ class ControlPH extends Controller
 
     public function postFoto(Request $request)
     {
-        //return "asddadas";
+        //return 'asddadas';
         if ($request->hasFile('foto')) {
-            $path = $request->file('foto')->store("piscina", 's3');
-            Storage::disk('s3')->url($path);
+            $path = $request->file('foto')->store('piscina', 's3');
+            $urlPH=Storage::disk('s3')->url($path);
+            $urlCL=Storage::disk('s3')->url($path);
+            $DominantColorsPH=$this->ImagePropeties($urlPH);
+            $DominantColorsCL=$this->ImagePropeties($urlCL);
+            $PHinicial=$this->CalculatePH($DominantColorsPH);
+            $CLinicial=$this->CalculateCL($DominantColorsCL);
             return response()->json(['message' => 'Archivos Subidos con exito']);
         }
 
         return response()->json(['message' => 'Error al subir el archivo']);
     }
 
-    public function ImagePropeties()
+    public function ImagePropeties(Request $request)
     {
-        //return "hola";
+        $img=substr($request->url,38,strlen($request->url));
+        return $img;
         $client = new RekognitionClient([
             'region' => env('AWS_DEFAULT_REGION'),
             'version' => 'latest',
@@ -36,21 +42,50 @@ class ControlPH extends Controller
         $results = $client->detectLabels([
             'Image' => [
                 'S3Object' => [
-                    //'Bucket' => env('AWS_BUCKET'), //'sw1-proyects',
-                    'Name' => 'https://i.blogs.es/466006/screenshot_8094/1366_2000.jpeg' //$img1,
+                    'Bucket' => env('AWS_BUCKET'), //'sw1-proyects',
+                    'Name' => $img,//'piscina/deU3rkxk1Vw5BoI2MJYkKO6MRNMBF6myJDFPb3DC.jpg' 
                 ],
             ],
             'Features' => [
-                "IMAGE_PROPERTIES"
+                'IMAGE_PROPERTIES'
             ],
-            "Settings" => [
-                "ImageProperties" => [
-                    "MaxDominantColors" => 5
+            'Settings' => [
+                'ImageProperties' => [
+                    'MaxDominantColors' => 5
                 ]
             ]
         ]);
-
-        $resultLabels = $results->get('Labels');
-        return $resultLabels;
+        //return $results;
+        $resultLabels = $results->get('ImageProperties');
+        //return $resultLabels['DominantColors'];
+        return $resultLabels['DominantColors'];
     }
+
+    private function CalculatePH($colors){      
+        $ph=0;  
+        foreach ($colors as $color) {
+            if($color['CSSColor'] === 'mediumvioletred') {
+                $ph= 7.8;
+            }else if ($color['CSSColor'] === 'thistle') {
+                $ph= 7.6;
+            }else if ($color['CSSColor'] === 'palevioletred') {
+                $ph = 7.4;
+            }
+        }
+        return $ph;
+    }
+
+    private function CalculateCL($colors){        
+        $cl=0;
+        foreach ($colors as $color) {
+            if ($color['CSSColor'] === 'saddlebrown') {
+                $cl= 3;
+            } else if ($color['CSSColor'] === 'sienna') {
+                $cl= 5;
+            }
+        }
+        return $cl;
+    }
+
+    
 }
